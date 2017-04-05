27877611ecd6350a377d1a0be4cdbad779d4e6e0
levan <levantsinadze@gmail.com>
2017-04-05 21:30:24 +0400

    implementing the gradient descents and backpropagation modules

---
+++ miniflow/quizes/gd.py
@@ -0,0 +1,14 @@
+"""
+Created on Apr 5, 2017
+
+@author: Levan Tsinadze
+"""
+def gradient_descent_update(x, gradx, learning_rate):
+    """
+    Performs a gradient descent update.
+    """
+    # TODO: Implement gradient descent.
+    
+    # Return the new value for x
+    return return x - learning_rate * gradx
+import f
--- miniflow/quizes/miniflow.py
+++ miniflow/quizes/miniflow.py
@@ -128,6 +128,29 @@ class Sigmoid(Node):
         # This is a dummy value to prevent numpy errors
         # if you test without changing this method.
         self.value = self._sigmoid(self.inbound_nodes[0].value)
+    
+    def backward(self):
+        """
+        Calculates the gradient using the derivative of
+        the sigmoid function.
+        """
+        # Initialize the gradients to 0.
+        self.gradients = {n: np.zeros_like(n.value) for n in self.inbound_nodes}
+
+        # Cycle through the outputs. The gradient will change depending
+        # on each output, so the gradients are summed over all outputs.
+        for n in self.outbound_nodes:
+            # Get the partial of the cost with respect to this node.
+            grad_cost = n.gradients[self]
+            """
+            TODO: Your code goes here!
+
+            Set the gradients property to the gradients with respect to each input.
+
+            NOTE: See the Linear node and MSE node for examples.
+            """
+            sigmoid = self.value
+            self.gradients[self.inbound_nodes[0]] += sigmoid * (1 - sigmoid) * grad_cost
         
 class MSE(Node):
     def __init__(self, y, a):
